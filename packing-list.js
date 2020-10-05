dc682b84d121a0a175630677fab0679845e5776f
RongY <yang.ariel@hotmail.com>
2020-10-05 14:49:07 -0400

    add new js

---
+++ packing-list.js
@@ -0,0 +1,13 @@
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
